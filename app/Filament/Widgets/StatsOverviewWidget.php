150a9ec0584a9b27125c484797ee52e4aa65a344
Wildan <cuinc99@gmail.com>
2024-12-21 13:12:45 +0800

    update stats card color

--- app/Filament/Widgets/StatsOverviewWidget.php
+++ app/Filament/Widgets/StatsOverviewWidget.php
@@ -18,11 +18,6 @@ class StatsOverviewWidget extends BaseWidget
 
     protected static ?int $sort = 0;
 
-    // protected function getColumns(): int
-    // {
-    //     return 12;
-    // }
-
     protected function getStats(): array
     {
         $transactions = Transaction::query()
@@ -47,70 +42,94 @@ class StatsOverviewWidget extends BaseWidget
                 fn(Builder $query, $date): Builder => $query->whereDate('purchase_date', '<=', $date),
             );
 
-        $transactionQuantity = Trend::model(Transaction::class)
-            ->dateColumn('purchase_date')
-            ->between(
-                start: now()->subYear(),
-                end: now(),
-            )
-            ->perMonth()
-            ->sum('quantity');
+        // $transactionQuantity = Trend::model(Transaction::class)
+        //     ->dateColumn('purchase_date')
+        //     ->between(
+        //         start: now()->subYear(),
+        //         end: now(),
+        //     )
+        //     ->perMonth()
+        //     ->sum('quantity');
 
-        $transactionTotalSales = Trend::model(Transaction::class)
-            ->dateColumn('purchase_date')
-            ->between(
-                start: now()->subYear(),
-                end: now(),
-            )
-            ->perMonth()
-            ->sum('subtotal_after_discount');
+        // $transactionTotalSales = Trend::model(Transaction::class)
+        //     ->dateColumn('purchase_date')
+        //     ->between(
+        //         start: now()->subYear(),
+        //         end: now(),
+        //     )
+        //     ->perMonth()
+        //     ->sum('subtotal_after_discount');
 
-        $transactionProfit = Trend::model(Transaction::class)
-            ->dateColumn('purchase_date')
-            ->between(
-                start: now()->subYear(),
-                end: now(),
-            )
-            ->perMonth()
-            ->sum('profit');
+        // $transactionProfit = Trend::model(Transaction::class)
+        //     ->dateColumn('purchase_date')
+        //     ->between(
+        //         start: now()->subYear(),
+        //         end: now(),
+        //     )
+        //     ->perMonth()
+        //     ->sum('profit');
 
         return [
             Stat::make(
                 label: __('models.transactions.title'),
-                value: number_format((clone $transactions)->count())),
+                value: number_format((clone $transactions)->count()))
+                ->icon('heroicon-o-shopping-bag')
+                ->extraAttributes([
+                    'class' => 'bg-transaction',
+                ]),
             Stat::make(
                 label: __('models.common.sold'),
                 value: number_format((clone $transactions)->sum('quantity')))
-                ->chart(
-                    $transactionQuantity
-                        ->map(fn(TrendValue $value) => $value->aggregate)
-                        ->toArray()
-                )
-                ->color('warning'),
+                // ->chart(
+                //     $transactionQuantity
+                //         ->map(fn(TrendValue $value) => $value->aggregate)
+                //         ->toArray()
+                // )
+                ->color('warning')
+                ->icon('heroicon-o-chart-bar')
+                ->extraAttributes([
+                    'class' => 'bg-sold',
+                ]),
             Stat::make(
                 label: __('models.transactions.fields.is_paid_options.paid'),
-                value: __("Rp. " . number_format((clone $transactions)->where('is_paid', true)->sum('subtotal_after_discount'), 0, ',', '.'))),
+                value: __("Rp. " . number_format((clone $transactions)->where('is_paid', true)->sum('subtotal_after_discount'), 0, ',', '.')))
+                ->icon('heroicon-o-check-circle')
+                ->extraAttributes([
+                    'class' => 'bg-paid',
+                ]),
             Stat::make(
                 label: __('models.transactions.fields.is_paid_options.unpaid'),
-                value: __("Rp. " . number_format((clone $transactions)->where('is_paid', false)->sum('subtotal_after_discount'), 0, ',', '.'))),
+                value: __("Rp. " . number_format((clone $transactions)->where('is_paid', false)->sum('subtotal_after_discount'), 0, ',', '.')))
+                ->icon('heroicon-o-x-circle')
+                ->extraAttributes([
+                    'class' => 'bg-unpaid',
+                ]),
             Stat::make(
                 label: __('models.transactions.fields.total_sales'),
                 value: __("Rp. " . number_format((clone $transactions)->sum('subtotal_after_discount'), 0, ',', '.')))
-                ->chart(
-                    $transactionTotalSales
-                        ->map(fn(TrendValue $value) => $value->aggregate)
-                        ->toArray()
-                )
-                ->color('info'),
+                // ->chart(
+                //     $transactionTotalSales
+                //         ->map(fn(TrendValue $value) => $value->aggregate)
+                //         ->toArray()
+                // )
+                ->color('info')
+                ->icon('heroicon-o-currency-dollar')
+                ->extraAttributes([
+                    'class' => 'bg-sales',
+                ]),
             Stat::make(
                 label: __('models.transactions.fields.profit'),
                 value: __("Rp. " . number_format((clone $transactions)->sum('profit'), 0, ',', '.')))
-                ->chart(
-                    $transactionProfit
-                        ->map(fn(TrendValue $value) => $value->aggregate)
-                        ->toArray()
-                )
-                ->color('success'),
+                // ->chart(
+                //     $transactionProfit
+                //         ->map(fn(TrendValue $value) => $value->aggregate)
+                //         ->toArray()
+                // )
+                ->color('success')
+                ->icon('heroicon-o-presentation-chart-line')
+                ->extraAttributes([
+                    'class' => 'bg-profit',
+                ]),
         ];
     }
 }
